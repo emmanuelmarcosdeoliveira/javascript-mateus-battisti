041fb6fcca9b6afc10a296e4f24c80294b590b66
Emmanuel Marcos de Oliveira <oliveira_emmanuel@outlook.com>
2023-04-14 23:37:56 -0300

    Module 02 finish

---
+++ modulo-02/9_operador_ternário/scripts.js
@@ -0,0 +1,10 @@
+// Operadores ternários
+// são comparados os valores lógicos onde se a a primeira expressão é verdaeira ele retorna o valor depois do Simbolo ( ? )  se for falso ele retorna o valor depois desse simbolo( : )
+
+// Essa comparação nos retorna um valor boolean ( veradeiro o u falso)
+
+// Exemplo:
+
+console.log(5 > 3 ? "É sim" : "É não");
+console.log(5 > 8 ? "É sim" : "É não");
+console.log("Emmanuel" == "Emmanuel" ? "Olá Emmanuel" : "Não é o Emmanuel")
