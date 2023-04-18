71e63a2277c41c67c94dd9dd60b31d0dae92bf60
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-04-18 11:03:25 -0300

    module 04 update

---
+++ modulo-03/exercicio_02/scritps.js
@@ -0,0 +1,6 @@
+console.log(10) 
+// Strign com numero Inteiro 
+console.log(10.54)
+// String com Numero decimal
+console.log(3*(12+04)/8)
+// string aritmética(uma soma) 
---
+++ modulo-04/5_prompt/scripts.js
@@ -0,0 +1,5 @@
+let nome = prompt("Qual é o seu nome?");
+// Chamamos a função prompt e atribuimos uma string  -"Qual é o seu nome" a ela, faznedo ela guardar a resposta do prompt dentro da memória do computador.
+console.log(`O seu nome é ${nome}`);
+let idade = prompt("Qual a sua idade?");
+console.log(idade);
