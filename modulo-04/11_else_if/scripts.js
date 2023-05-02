2797679711cf591cb5537ab12b642679e347a33c
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-05-02 00:12:29 -0300

    Modulo 04 - Video 61

---
+++ modulo-04/11_else_if/scripts.js
@@ -0,0 +1,18 @@
+let nome = 'Emmanuel'
+let idade = 43;
+
+
+if(nome != undefined && nome == 'Jonas'){
+  console.log('Nome está definido')
+}else if (nome == 'Emmanuel' && nome.length > 5 && idade == 50){
+console.log('O nome é Emmanuel')
+}else {
+  console.log('Não é Emmanuel!')
+}
+
+
+if(1 > 2){
+  console.log('Teste')
+}else if(1==1){
+  console.log('Testando')
+}
---
+++ modulo-04/8_consolelog/scripts.js
@@ -0,0 +1,6 @@
+let nome = 'Emmanuel'
+let idade = 44;
+
+console.log(nome)
+console.log(idade)
+console.log(`Meu nome é ${nome}, e eu tenho ${idade} anos de idade`)
