6b5ec73d6363d2d85e022a1ebd2ab912c1d15dab
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2023-06-07 13:39:20 -0300

    Update

---
+++ modulo-04/1_variaveis/scripts.js
@@ -0,0 +1,17 @@
+// os códigos em JavaScript são executados de cima para baixo;
+
+let nome = 'Emmanuel';
+console.log(nome);
+console.log(`O me nome é ${nome}`)
+
+let laranjas = 5;
+console.log(`Na Sexta temos: ${laranjas } laranjas`)
+console.log(laranjas  * laranjas)
+nome = "João"
+console.log(nome)
+laranja  = 83284;
+console.log(laranja)
+laranja = "laranja"; // atribuindo valor sem a delcartação let (não recomendado)
+console.log(laranja)
+let um =1, dois = 2, tres = 3;
+console.log(um + dois + tres)
---
+++ modulo-04/4_reservadas/scripts.js
@@ -0,0 +1,8 @@
+// let if = 'Teste' Não é possivel fazer essa declaração pois a plavra (if) é uma plavara reservada.
+// let function = 'Teste' ão é possivel fazer essa declaração pois a plavra (if) é uma plavara reservada.
+let functionTest = "teste";
+let function1 = "teste2";
+/* podemos usar palavras reservadas acrescentado alguma palavras após conforme exemplo acima. */
+
+console.log(functionTest);
+console.log(function1);
